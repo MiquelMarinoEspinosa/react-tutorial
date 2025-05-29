20de6b2bd792cbec10967e98a1ae65cececd5b17
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-29 09:18:54 +0000

    introducing props

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -3,10 +3,7 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Rob" message="Hi there!" />
     </div>
   );
 }
