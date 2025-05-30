8d55c309defbb01e371bd933c5aca38f19a0fce5
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-30 09:31:41 +0000

    filter fruits by price and use fragments

--- reactpro/src/components/Fruit.jsx
+++ reactpro/src/components/Fruit.jsx
@@ -1,7 +1,13 @@
 export default function Fruit({ name, price, emoji }) {
   return (
-    <li key={name}>
-      {emoji} {name} ${price}
-    </li>
+    <>
+      {price > 5 ? (
+        <li key={name}>
+          {emoji} {name} ${price}
+        </li>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
