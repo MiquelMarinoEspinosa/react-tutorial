533b6c114725067365480addbcc440ea672e9cf5
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-30 09:37:06 +0000

    filter fruits by soldout

--- reactpro/src/components/Fruit.jsx
+++ reactpro/src/components/Fruit.jsx
@@ -1,13 +1,9 @@
-export default function Fruit({ name, price, emoji }) {
+export default function Fruit({ name, price, emoji, soldout }) {
   return (
     <>
-      {price > 5 ? (
-        <li key={name}>
-          {emoji} {name} ${price}
-        </li>
-      ) : (
-        ""
-      )}
+      <li key={name}>
+        {emoji} {name} ${price} {soldout ? "soldOut" : ""}
+      </li>
     </>
   );
 }
--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -3,11 +3,11 @@ import Fruit from "./Fruit";
 export default function Fruits() {
   //const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
   const fruits = [
-    { name: "Apple", price: 10, emoji: "🍎" },
-    { name: "Mango", price: 7, emoji: "🥭" },
-    { name: "Banana", price: 2, emoji: "🍌" },
-    { name: "Orange", price: 5, emoji: "🍊" },
-    { name: "Pineapple", price: 8, emoji: "🍍" },
+    { name: "Apple", price: 10, emoji: "🍎", soldout: true },
+    { name: "Mango", price: 7, emoji: "🥭", soldout: false },
+    { name: "Banana", price: 2, emoji: "🍌", soldout: true },
+    { name: "Orange", price: 5, emoji: "🍊", soldout: false },
+    { name: "Pineapple", price: 8, emoji: "🍍", soldout: true },
   ];
 
   return (
@@ -19,6 +19,7 @@ export default function Fruits() {
             name={fruit.name}
             emoji={fruit.emoji}
             price={fruit.price}
+            soldout={fruit.soldout}
           />
         ))}
       </ul>
